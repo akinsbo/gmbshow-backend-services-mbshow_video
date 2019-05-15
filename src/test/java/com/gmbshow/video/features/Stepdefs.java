e01e1591e5497c32f3f24007e6cb2891e3ebec7e
Olaolu Akinsete <akinsbo@gmail.com>
2019-05-15 22:51:30 +0100

    setup cucumber

---
+++ src/test/java/com/gmbshow/video/features/Stepdefs.java
@@ -0,0 +1,29 @@
+package com.gmbshow.video.features;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class Stepdefs {
+
+    @Given("I open a browser")
+    public void i_open_a_browser() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I navigate to google page")
+    public void i_navigate_to_google_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I validate the search text field")
+    public void i_validate_the_search_text_field() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+}
